d76dcd3afe6783fef786425484965661ab7a4162
Roelf <roelf.diedericks>
2024-07-25 14:58:52 +0200

    Finished task 3. Added all relationships to cases and policys.

--- sugarcrm/web/modules/lt_case/vardefs.php
+++ sugarcrm/web/modules/lt_case/vardefs.php
@@ -7,7 +7,22 @@ $moduleVardefs['lt_case'] = [
     'label' => 'Case',
     'detail_view_class' => 'CaseDetailView',
     'edit_view_class' => 'CaseEditView',
-    'related_modules' => [],
+    'related_modules' => [
+        'lt_policy' => [
+            'relationship_type' => 'one-to-many',
+            'relation_key_rhs' => 'id',
+        ],
+        'lt_customer' => [
+            'relationship_type' => 'many-to-many',
+            'join_table' => 'customers_cases',
+            'join_key_lhs' => 'customer_id',
+            'join_key_rhs' => 'case_id',
+        ],
+        'lt_case' => [
+            'relationship_type' => 'one-to-many',
+            'relation_key_rhs' => 'case_id',
+        ],
+    ],
     /* Add all the fields for your module/table below */
     'fields' => [
         /*Any definitions added to the `id` field will be ignored, leave it as an empty array*/
