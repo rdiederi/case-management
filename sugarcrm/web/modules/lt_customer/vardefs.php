cf6d85ed76767d47d0fe33b4439e316ab595ad94
Roelf <roelf.diedericks>
2024-07-25 14:39:28 +0200

    Fixed the customer list, detail and edit screens.

--- sugarcrm/web/modules/lt_customer/vardefs.php
+++ sugarcrm/web/modules/lt_customer/vardefs.php
@@ -13,13 +13,39 @@ $moduleVardefs['lt_customer'] = [
     'fields' => [
         /*Any definitions added to the `id` field will be ignored, leave it as an empty array*/
         'id' => [],
-         //Add more fields here
+        'first_name' => [
+            'type' => 'string',
+            'null' => false,
+            'length' => 255,
+            'default' => ''
+        ],
+        'last_name' => [
+            'type' => 'string',
+            'null' => false,
+            'length' => 255,
+            'default' => ''
+        ],
+        'id_number' => [
+            'type' => 'string',
+            'null' => false,
+            'length' => 15,
+            'default' => ''
+        ],
+        'phone' => [
+            'type' => 'string',
+            'null' => true,
+            'length' => 15,
+            'default' => ''
+        ],
+
     ],
     'detail_view_fields' => [
-        ['name', 'description'],
+        ['first_name', 'last_name'],
+        ['id_number', 'phone'],
     ],
     'edit_view_fields' => [
-        ['name', 'description'],
+        ['first_name', 'last_name'],
+        ['id_number', 'phone'],
     ],
-    'list_view_fields' => ['name', 'description'],
+    'list_view_fields' => ['first_name', 'last_name', 'description'],
 ];
